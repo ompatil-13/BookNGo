95d7f3b2d120d91000a1ef9eafa106adc879cf90
ompatil-13 <151327445+ompatil-13@users.noreply.github.com>
2025-10-30 20:55:32 +0530

    Refactor server.js to improve dotenv usage

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
+
 
 import userRoutes from "./routes/userRoutes.js";
 import ticketRoutes from "./routes/ticketRoutes.js";
@@ -27,5 +29,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/tickets", ticketRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
